2df25995352be2def090504c74575704108b8c58
unknown <blagii009@gmail.com>
2026-01-28 20:01:41 +0100

    Add STUN servers for WebRTC

--- client/src/components/VideoChat.jsx
+++ client/src/components/VideoChat.jsx
@@ -87,6 +87,12 @@ const VideoChat = () => {
             initiator: true,
             trickle: false,
             stream: stream,
+            config: {
+                iceServers: [
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                ],
+            },
         });
 
         peer.on('signal', (data) => {
@@ -120,6 +126,12 @@ const VideoChat = () => {
             initiator: false,
             trickle: false,
             stream: stream,
+            config: {
+                iceServers: [
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                ],
+            },
         });
 
         peer.on('signal', (data) => {
